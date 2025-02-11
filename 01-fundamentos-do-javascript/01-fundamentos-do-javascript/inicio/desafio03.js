a38398923ca92a94cef3d31863b9da7a648a539b
Ricardo <cadimrosalem@gmail.com>
2025-02-11 11:23:58 -0300

    desafio da fruta

---
+++ 01-fundamentos-do-javascript/01-fundamentos-do-javascript/inicio/desafio03.js
@@ -0,0 +1,11 @@
+const fruta = 'abacaxi';
+
+if (fruta === 'maçã'){
+    console.log(`A fruta é uma ${fruta}!`);
+} else if (fruta === 'laranja'){
+    console.log(`A fruta é uma ${fruta}!`);
+} else if (fruta === 'banana'){
+    console.log(`A fruta é uma ${fruta}!`);
+} else{
+    console.log(`A fruta não é maçã, nem banana, nem laranja, é ${fruta}!`);
+}
