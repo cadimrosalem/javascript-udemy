7e55311a757ea56ec52a4c5b1807868ddddb238f
Ricardo <cadimrosalem@gmail.com>
2025-03-06 11:10:10 -0300

    desafio portas mágicas

---
+++ 01-fundamentos-do-javascript/01-fundamentos-do-javascript/inicio/desafio06.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Desafio 6
+
+let porta1 = false;
+let porta2 = false;
+let porta3 = false;
+
+function abrirPorta1(){
+    porta1 = true;
+}
+
+function abrirPorta2(){
+    porta2 = true;
+}
+
+function abrirPorta3(){
+    porta3 = true;
+}
+
+function verTes(){
+    if (porta1 && porta2 && porta3){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function res(){
+    if (verTes()){
+        console.log('Você encontrou o tesouro! Parabéns!');
+    } else{
+        console.log('Continue procurando!');
+    }
+}
+
+abrirPorta1();
+abrirPorta2();
+abrirPorta3();
+res();
+
+
+
+
+
+
