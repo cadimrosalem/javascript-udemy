06047e30a8c78f0a838bdbce29d4c05bab4ae33b
Ricardo <cadimrosalem@gmail.com>
2025-03-11 12:37:48 -0300

    desafio 8 - array de chaves

---
+++ 01-fundamentos-do-javascript/01-fundamentos-do-javascript/inicio/desafio08.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/*
+let chaves = ["chave1", "chave2", "chave3"];
+let portaMisteriosa = "chave1"
+
+if (chaves.includes(portaMisteriosa)){
+    console.log("Você encontrou a chave certa! A porta está aberta!");
+} else{
+    console.log("Essa chave não abre a porta. Continue procurando!");
+}
+*/
+
+/*
+let chaves = [];
+
+chaves.push("chave 1");
+chaves.push("chave 2");
+chaves.push("chave 3");
+
+console.log(chaves);
+
+let portaMisteriosa = "chave 1";
+
+if (chaves.includes(portaMisteriosa)){
+    console.log("Você encontrou a chave certa! A porta está aberta!");
+} else{
+    console.log("Essa chave não abre a porta. Continue procurando!");
+}
+*/
+
+
+
+
