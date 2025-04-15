4346c36229b09348352a81b0d51d04fc59c33daa
Ricardo <cadimrosalem@gmail.com>
2025-04-15 14:40:41 -0300

    como debugar com o google chrome

--- 01-fundamentos-do-javascript/01-fundamentos-do-javascript/inicio/script.js
+++ 01-fundamentos-do-javascript/01-fundamentos-do-javascript/inicio/script.js
@@ -1608,3 +1608,59 @@
        console.log('Parabéns!');
        console.log('Você completou todas as voltas da corrida!');
        */
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(2, 3));
+
+function subtract(a, b) {
+  return a - b;
+}
+
+console.log(subtract(3, 2));
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(5, 5));
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(divide(10, 2));
+
+function calculator(operation, x, y) {
+  let result;
+
+  switch (operation) {
+    case "add":
+      result = add(x, y);
+      console.log("Resultado da adição", result);
+      break;
+    case "subtract":
+      result = subtract(x, y);
+      console.log("Resultado da subtração", result);
+      break;
+    case "multiply":
+      // Vamos adicionar um breakpoint aqui para depuração
+      result = multiply(x, y);
+      console.log("Resultado da multiplicação", result);
+      break;
+    case "divide":
+      result = divide(x, y);
+      console.log("Resultado da divisão", result);
+      break;
+    default:
+      console.log("Operação inválida");
+  }
+}
+
+calculator("add", 10, 5);
+calculator("subtract", 20, 8);
+debugger;
+calculator("multiply", 7, 3);
+calculator("divide", 100, 4);
