60ee057caa79836d81ec85a22ae06f62e4ffa8dc
Ricardo <cadimrosalem@gmail.com>
2025-05-05 12:08:01 -0300

    executando eventos

--- 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
+++ 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
@@ -1,4 +1,17 @@
 "use strict"; // Define o modo estrito para evitar erros comuns.
 
-console.log(document.querySelector("#convert-button").textContent);
+const meuBotao = document.getElementById("convert-button");
+const result = document.getElementById("result");
+console.log(meuBotao.textContent);
 
+// Adicionar evento de alerta quando o mouse passar por cima do botão
+meuBotao.addEventListener("mouseover", 
+    function () {
+  result.textContent = "Mouse por cima";
+});
+
+// Adicionar evento de alerta quando o mouse sair de cima do botão
+meuBotao.addEventListener("mouseout", 
+    function () {
+  result.textContent = "";
+});
