6f96d6d88801ae5becde78f63a2850c67d8d56a5
Ricardo <cadimrosalem@gmail.com>
2025-05-06 11:56:15 -0300

    implementando a lógica da calculadora

--- 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
+++ 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
@@ -1,17 +1,45 @@
 "use strict"; // Define o modo estrito para evitar erros comuns.
 
-const meuBotao = document.getElementById("convert-button");
-const result = document.getElementById("result");
-console.log(meuBotao.textContent);
+// Eventos
+const convertButton = document.getElementById("convert-button");
 
-// Adicionar evento de alerta quando o mouse passar por cima do botão
-meuBotao.addEventListener("mouseover", 
-    function () {
-  result.textContent = "Mouse por cima";
-});
+// Mapear o evento do Click
+convertButton.addEventListener("click", function () {
+  const result = document.getElementById("result");
+  const valueToConvert = parseFloat(
+    document.getElementById("value-to-convert").value
+  );
 
-// Adicionar evento de alerta quando o mouse sair de cima do botão
-meuBotao.addEventListener("mouseout", 
-    function () {
-  result.textContent = "";
+  const unitSelector = document.getElementById("unit-selector").value;
+
+  let calculated;
+
+  switch (unitSelector) {
+    case "cm":
+      // lógica caso o valor seja CM
+      calculated = valueToConvert * 0.393701;
+      result.textContent = `${valueToConvert} cm é igual a ${calculated.toFixed(2)} polegadas`
+      break;
+
+    case "in":
+      // lógica caso o valor seja IN
+      calculated = valueToConvert * 2.54;
+      result.textContent = `${valueToConvert} in é igual a ${calculated.toFixed(2)} centímetos`
+      break;
+
+    case "m":
+      // lógica caso o valor seja M
+      calculated = valueToConvert * 39.3701;
+      result.textContent = `${valueToConvert} m é igual a ${calculated.toFixed(2)} polegadas`
+      break;
+
+    case "ft":
+      // lógica caso o valor seja FT
+      calculated = valueToConvert * 30.48;
+      result.textContent = `${valueToConvert} ft é igual a ${calculated.toFixed(2)} centímetos`
+      break;
+
+    default:
+      // Nenhuma das opções
+  }
 });
