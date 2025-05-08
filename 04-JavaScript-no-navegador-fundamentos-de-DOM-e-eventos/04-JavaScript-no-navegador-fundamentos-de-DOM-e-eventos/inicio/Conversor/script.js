a8983c0d240c244aabc9c6f93992af58939891e6
Ricardo <cadimrosalem@gmail.com>
2025-05-08 17:04:32 -0300

    desafio 18 - criando um histórico de conversões

--- 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
+++ 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
@@ -5,6 +5,7 @@ const convertButton = document.getElementById("convert-button");
 
 // Mapear o evento do Click
 convertButton.addEventListener("click", function () {
+  const historyText = document.getElementById("historyText");
   const result = document.getElementById("result");
   const valueToConvert = parseFloat(
     document.getElementById("value-to-convert").value
@@ -27,28 +28,36 @@ convertButton.addEventListener("click", function () {
     case "cm":
       // lógica caso o valor seja CM
       calculated = valueToConvert * 0.393701;
-      result.textContent = `${valueToConvert} cm é igual a ${calculated.toFixed(2)} polegadas`
+      result.textContent = `${valueToConvert} cm é igual a ${calculated.toFixed(2)} polegadas`;
       result.style.color = "blue";
       result.classList.remove(".redMessage");
       result.classList.add(".blueMessage");
+
+      historyText.innerText = historyText.innerText + "\n" + result.textContent;
       break;
 
     case "in":
       // lógica caso o valor seja IN
       calculated = valueToConvert * 2.54;
-      result.textContent = `${valueToConvert} in é igual a ${calculated.toFixed(2)} centímetos`
+      result.textContent = `${valueToConvert} in é igual a ${calculated.toFixed(2)} centímetos`;  
+      
+      historyText.innerText = historyText.innerText + "\n" + result.textContent;
       break;
 
     case "m":
       // lógica caso o valor seja M
       calculated = valueToConvert * 39.3701;
-      result.textContent = `${valueToConvert} m é igual a ${calculated.toFixed(2)} polegadas`
+      result.textContent = `${valueToConvert} m é igual a ${calculated.toFixed(2)} polegadas`;
+
+      historyText.innerText = historyText.innerText + "\n" + result.textContent;      
       break;
 
     case "ft":
       // lógica caso o valor seja FT
       calculated = valueToConvert * 30.48;
-      result.textContent = `${valueToConvert} ft é igual a ${calculated.toFixed(2)} centímetos`
+      result.textContent = `${valueToConvert} ft é igual a ${calculated.toFixed(2)} centímetos`;
+      
+      historyText.innerText = historyText.innerText + "\n" + result.textContent;
       break;
 
     default:
