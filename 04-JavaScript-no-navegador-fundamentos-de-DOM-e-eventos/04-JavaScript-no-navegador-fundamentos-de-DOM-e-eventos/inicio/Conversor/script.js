abdf91f0dbf3f82875c812f9825c3985c28b9d6f
Ricardo <cadimrosalem@gmail.com>
2025-05-12 12:56:06 -0300

    desafio 19 - botão de limpeza

--- 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
+++ 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
@@ -1,17 +1,30 @@
 "use strict"; // Define o modo estrito para evitar erros comuns.
 
-// Eventos
+
+// Capturar os elementos
+const historyText = document.getElementById("historyText");
+const result = document.getElementById("result");
 const convertButton = document.getElementById("convert-button");
+const clearButton = document.getElementById("clear-button")
+const unitSelector = document.getElementById("unit-selector");
+
+// Mapear o click do botão de limpeza
+clearButton.addEventListener("click", function() {
+  historyText.innerText = "";
+  result.innerText = "";
+  document.getElementById("value-to-convert").value = "";
+  unitSelector.options.selectedIndex = 0;
+});
 
 // Mapear o evento do Click
-convertButton.addEventListener("click", function () {
+convertButton.addEventListener("click", function() {    
   const historyText = document.getElementById("historyText");
   const result = document.getElementById("result");
   const valueToConvert = parseFloat(
     document.getElementById("value-to-convert").value
   );
 
-  const unitSelector = document.getElementById("unit-selector").value;
+  
 
   if (valueToConvert === 0 || valueToConvert === "" || isNaN(valueToConvert)){
     result.textContent = "Informe um valor diferente de 0";
@@ -24,7 +37,7 @@ convertButton.addEventListener("click", function () {
 
   let calculated;
 
-  switch (unitSelector) {
+  switch (unitSelector.value) {
     case "cm":
       // lógica caso o valor seja CM
       calculated = valueToConvert * 0.393701;
@@ -33,31 +46,31 @@ convertButton.addEventListener("click", function () {
       result.classList.remove(".redMessage");
       result.classList.add(".blueMessage");
 
-      historyText.innerText = historyText.innerText + "\n" + result.textContent;
+      historyText.innerText = historyText.innerText + "\n" + result.textContent;   
       break;
 
     case "in":
       // lógica caso o valor seja IN
       calculated = valueToConvert * 2.54;
-      result.textContent = `${valueToConvert} in é igual a ${calculated.toFixed(2)} centímetos`;  
+      result.textContent = `${valueToConvert} in é igual a ${calculated.toFixed(2)} centímetos`;   
       
-      historyText.innerText = historyText.innerText + "\n" + result.textContent;
+      historyText.innerText = historyText.innerText + "\n" + result.innerText;
       break;
 
     case "m":
       // lógica caso o valor seja M
       calculated = valueToConvert * 39.3701;
-      result.textContent = `${valueToConvert} m é igual a ${calculated.toFixed(2)} polegadas`;
-
-      historyText.innerText = historyText.innerText + "\n" + result.textContent;      
+      result.textContent = `${valueToConvert} m é igual a ${calculated.toFixed(2)} polegadas`; 
+      
+      historyText.innerText = historyText.innerText + "\n" + result.innerText;
       break;
 
     case "ft":
       // lógica caso o valor seja FT
       calculated = valueToConvert * 30.48;
-      result.textContent = `${valueToConvert} ft é igual a ${calculated.toFixed(2)} centímetos`;
-      
-      historyText.innerText = historyText.innerText + "\n" + result.textContent;
+      result.textContent = `${valueToConvert} ft é igual a ${calculated.toFixed(2)} centímetos`;     
+
+      historyText.innerHTML = historyText.innerText + "\n" + result.innerText;
       break;
 
     default:
@@ -65,3 +78,5 @@ convertButton.addEventListener("click", function () {
       result.textContent = `Selecione uma opção válida`;
   }
 });
+
+
