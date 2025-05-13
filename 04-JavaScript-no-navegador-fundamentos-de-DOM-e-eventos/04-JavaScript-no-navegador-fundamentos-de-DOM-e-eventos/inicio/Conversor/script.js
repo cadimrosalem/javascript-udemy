a8426d0b5dfcc3cd0cc11f8b990dbae3f0f94c27
Ricardo <cadimrosalem@gmail.com>
2025-05-13 11:31:58 -0300

    limpando o código com DRY

--- 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
+++ 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Conversor/script.js
@@ -1,15 +1,16 @@
 "use strict"; // Define o modo estrito para evitar erros comuns.
 
+// Dry (don´t repeat yourself / não repita você mesmo)
 
 // Capturar os elementos
 const historyText = document.getElementById("historyText");
 const result = document.getElementById("result");
 const convertButton = document.getElementById("convert-button");
-const clearButton = document.getElementById("clear-button")
+const clearButton = document.getElementById("clear-button");
 const unitSelector = document.getElementById("unit-selector");
 
 // Mapear o click do botão de limpeza
-clearButton.addEventListener("click", function() {
+clearButton.addEventListener("click", function () {
   historyText.innerText = "";
   result.innerText = "";
   document.getElementById("value-to-convert").value = "";
@@ -17,22 +18,32 @@ clearButton.addEventListener("click", function() {
 });
 
 // Mapear o evento do Click
-convertButton.addEventListener("click", function() {    
+convertButton.addEventListener("click", function () {
   const historyText = document.getElementById("historyText");
   const result = document.getElementById("result");
   const valueToConvert = parseFloat(
     document.getElementById("value-to-convert").value
   );
 
-  
-
-  if (valueToConvert === 0 || valueToConvert === "" || isNaN(valueToConvert)){
+  if (valueToConvert === 0 || valueToConvert === "" || isNaN(valueToConvert)) {
     result.textContent = "Informe um valor diferente de 0";
-    result.style.backgroundColor = "red";
-    result.style.color = "white";
-    result.classList.remove(".blueMessage");
-    result.classList.add(".redMessage");
+    result.style.color = "red";
+    result.classList.add(".normalMessage");
+    result.classList.remove(".errorMessage");
+    refreshScreen(valueToConvert, calculated);
     return;
+  }
+
+  function refreshScreen(valueToConvert, calculated, unit, unitOrigin) {
+    result.classList.remove(".errorMessage");
+    result.classList.add(".normalMessage");
+    result.style.color = "blue";
+
+    result.textContent = `${valueToConvert} ${unitOrigin} é igual a ${calculated.toFixed(
+      2
+    )} ${unit}`;
+
+    historyText.innerText = historyText.innerText + "\n" + result.textContent;
   }
 
   let calculated;
@@ -41,36 +52,25 @@ convertButton.addEventListener("click", function() {
     case "cm":
       // lógica caso o valor seja CM
       calculated = valueToConvert * 0.393701;
-      result.textContent = `${valueToConvert} cm é igual a ${calculated.toFixed(2)} polegadas`;
-      result.style.color = "blue";
-      result.classList.remove(".redMessage");
-      result.classList.add(".blueMessage");
-
-      historyText.innerText = historyText.innerText + "\n" + result.textContent;   
+      refreshScreen(valueToConvert, calculated, "polegadas", unitSelector.value);
       break;
 
     case "in":
       // lógica caso o valor seja IN
       calculated = valueToConvert * 2.54;
-      result.textContent = `${valueToConvert} in é igual a ${calculated.toFixed(2)} centímetos`;   
-      
-      historyText.innerText = historyText.innerText + "\n" + result.innerText;
+      refreshScreen(valueToConvert, calculated, "centímetros", unitSelector.value);
       break;
 
     case "m":
       // lógica caso o valor seja M
       calculated = valueToConvert * 39.3701;
-      result.textContent = `${valueToConvert} m é igual a ${calculated.toFixed(2)} polegadas`; 
-      
-      historyText.innerText = historyText.innerText + "\n" + result.innerText;
+      refreshScreen(valueToConvert, calculated, "polegadas", unitSelector.value);
       break;
 
     case "ft":
       // lógica caso o valor seja FT
       calculated = valueToConvert * 30.48;
-      result.textContent = `${valueToConvert} ft é igual a ${calculated.toFixed(2)} centímetos`;     
-
-      historyText.innerHTML = historyText.innerText + "\n" + result.innerText;
+      refreshScreen(valueToConvert, calculated, "centímetros", unitSelector.value);
       break;
 
     default:
@@ -78,5 +78,3 @@ convertButton.addEventListener("click", function() {
       result.textContent = `Selecione uma opção válida`;
   }
 });
-
-
