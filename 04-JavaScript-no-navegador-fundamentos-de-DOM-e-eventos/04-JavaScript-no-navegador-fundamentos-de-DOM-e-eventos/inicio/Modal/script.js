94e5bb1c73f63f587e78b11b2608916f0117e47c
Ricardo <cadimrosalem@gmail.com>
2025-05-16 13:20:55 -0300

    inteceptar as teclas do teclado pelo Javascript

--- 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Modal/script.js
+++ 04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/04-JavaScript-no-navegador-fundamentos-de-DOM-e-eventos/inicio/Modal/script.js
@@ -12,47 +12,52 @@ const closeBtn = document.getElementsByClassName("close")[0];
 const overlay = document.getElementById("overlay");
 const content = document.getElementById("content");
 
-function showModal(textToShow){
-    console.log("botão clicado");
-    modal.style.display = "block";  
-    overlay.style.display = "block";
-    content.style.filter = "blur(2px)"; 
-    modalText.innerText = textToShow;
+function showModal(textToShow) {
+  console.log("botão clicado");
+  modal.style.display = "block";
+  overlay.style.display = "block";
+  content.style.filter = "blur(2px)";
+  modalText.innerText = textToShow;
 }
 
-// Evento de clique para abrir o botão 
-openModalBtn1.addEventListener("click", function() {
-    showModal("Este é o modal 1.\n Pressione Sim ou Não");
+// Evento de clique para abrir o botão
+openModalBtn1.addEventListener("click", function () {
+  showModal("Este é o modal 1.\n Pressione Sim ou Não");
 });
 
-// Evento de clique para abrir o botão 
-openModalBtn2.addEventListener("click", function() {
-    showModal("Este é o modal 2.\n Pressione Sim ou Não");
+// Evento de clique para abrir o botão
+openModalBtn2.addEventListener("click", function () {
+  showModal("Este é o modal 2.\n Pressione Sim ou Não");
 });
 
 function closeModal() {
-    modal.style.display = "none";  
-    overlay.style.display = "none";
-    content.style.filter = "blur(0px)"; 
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  content.style.filter = "blur(0px)";
 }
 
 // Evento do botão fechar
 closeBtn.addEventListener("click", function() {
-    console.log("Botão fechar foi clicado");
-    closeModal();
+  console.log("Botão fechar foi clicado");
+  closeModal();
 });
 
 // Evento do click do Sim
-yesBtn.addEventListener("click", function(){
-    console.log("Botão Sim foi pressionado");
-    closeModal();
+yesBtn.addEventListener("click", function() {
+  console.log("Botão Sim foi pressionado");
+  closeModal();
 });
 
 // Evento do click do Não
-noBtn.addEventListener("click", function(){
-    console.log("Botão Não foi pressionado");
-    closeModal();
+noBtn.addEventListener("click", function() {
+  console.log("Botão Não foi pressionado");
+  closeModal();
 });
 
-
-
+// Função para pegar tecla pressionada
+window.addEventListener("keydown", function(e) {
+    if (e.key === "Escape" && modal.style.display === "block"){
+         console.log("Esc pressionado");
+         closeModal();
+    }
+});
